f52687ddaa8bf045be90eb013039f273e1ef73bd
Abdel Raoof <olakara@gmail.com>
2022-06-18 10:47:19 +0400

    feat(dashboard): implemented basic counter widget component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
+import CounterWidget from '../components/dashboard/counter-widget'
 
 export default function Home() {
+
+  const vm = {
+    title: 'Counter',
+    countValue: '24,600',
+  };
+
  return (<>
       <Head>
         <title>Home Page</title>
@@ -8,6 +15,10 @@ export default function Home() {
       <h1 className="p-4 text-3xl font-bold underline">
         Hello world!
       </h1>
+       <dl class="px-2 mt-5 grid grid-cols-2 gap-5 sm:grid-cols-6">
+        <CounterWidget vm={vm} />
+       </dl>
+      
     </>
   )
 }
